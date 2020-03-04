a1544465f69f00ef75a7bb7aab7fe9b2c8cce2dd
ricardo39985 <ricardo39985@gmail.com>
2020-03-03 20:02:36 -0500

    add pg gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,8 @@
 ENV['SINATRA_ENV'] ||= "development"
-
+require 'pg'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+set :database_file, './database.yml'
 
 require './app/controllers/application_controller'
 require 'pry'
